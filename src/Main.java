3efbbc2780cc8390486e768a0ea0af6d6c35368e
ravikiranreddyvanga <vjravikiranreddy@gmail.com>
2023-07-23 13:38:37 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+       List<TreeNode> list = solution.allPossibleFBT(7);
+    }
+}
